08351e32bbda024aa6640f1e5fb9a73f0ee3bc30
Claire <ruppelclaire@yahoo.de>
2024-10-05 14:26:12 +0200

    Remove typing from server file

--- server.ts
+++ server.ts
@@ -1,20 +1,20 @@
 const express = require("express")
 const next = require("next")
 
-const dev: boolean = process.env.NODE_ENV !== "production"
+const dev = process.env.NODE_ENV !== "production"
 const nextApp = next({ dev })
 const handle = nextApp.getRequestHandler()
 
-const port: number = 3454
+const port = 3454
 
 nextApp.prepare().then(() => {
   const app = express()
 
-  app.get("*", (req: any, res: any) => {
+  app.get("*", (req, res) => {
     return handle(req, res)
   })
 
-  app.listen(port, (err?: Error) => {
+  app.listen(port, (err) => {
     if (err) throw err
     console.log(`> Ready on localhost:${port}`)
   })
