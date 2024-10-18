4565877e4fda76e7cf26452624f9891405940937
Claire <ruppelclaire@yahoo.de>
2024-10-18 19:11:28 +0200

    🐛 Add logging to the server side

--- server.ts
+++ server.ts
@@ -1,6 +1,21 @@
 const express = require("express")
 const next = require("next")
 
+const fs = require('fs');
+const util = require('util');
+const log_file = fs.createWriteStream(__dirname + '/debug.log', {flags: 'w'});
+const log_stdout = process.stdout;
+
+console.log = (d, e, f, g) => {
+  log_file.write(util.format('LOG: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
+  log_stdout.write(util.format('LOG: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
+}
+
+console.error = (d, e, f, g) => {
+  log_file.write(util.format('ERROR: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
+  log_stdout.write(util.format('ERROR: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
+}
+
 const dev = process.env.NODE_ENV !== "production"
 const nextApp = next({ dev })
 const handle = nextApp.getRequestHandler()
